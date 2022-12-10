92fbded280a1f2dc665e2fa1540e9373a429edef
JCORP <info@jcorporationtech.com>
2022-12-10 09:53:50 +0900

    2022.12.03 complete

--- app/src/main/java/com/yjj/yjj1127/MainActivity.kt
+++ app/src/main/java/com/yjj/yjj1127/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.yjj.yjj1127
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.Surface
 import android.webkit.JavascriptInterface
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.yjj.yjj1127.databinding.ActivityMainBinding
 import java.util.*
@@ -30,20 +32,36 @@ class MainActivity : AppCompatActivity() {
         )
 
 
+        wv.loadUrl("file:///android_asset/webview1.html")
 
-
-
-
+//        Surface(color = MaterialTheme.colors.background) {
+//            Column {
+//                AndroidView(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .height(300.dp),
+//                    factory = { context ->
+//                        WebView(context).apply {
+//                            settings.javaScriptEnabled = true
+//                            setBackgroundColor(Color.TRANSPARENT)
+//                            loadUrl("file:///android_asset/webview1.html")
+//                            addJavascriptInterface(
+//                                JavascriptBridge(),
+//                                "Native"
+//                            )
+//                        }
+//                    }
+//                )
+//            }
+//        }
     }
-
-
-
 
 
     class JavascriptBridge(){
         @JavascriptInterface
         fun showMessage(){
             System.out.println("Message Received");
+            Toast.makeText(this@MainActivity, "Message Received", Toast.LENGTH_SHORT).show()
         }
     }
 
