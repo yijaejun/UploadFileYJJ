a260af71aa4802e36dbd16c984a56aa8822395a2
JCORP <info@jcorporationtech.com>
2022-12-10 09:42:17 +0900

    2022.12.03 complete

--- app/src/main/java/com/yjj/yjj1127/MainActivity.kt
+++ app/src/main/java/com/yjj/yjj1127/MainActivity.kt
@@ -1,34 +1,14 @@
 package com.yjj.yjj1127
-
-import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
-import android.util.Log
+import android.view.Surface
 import android.webkit.JavascriptInterface
-import android.webkit.ValueCallback
-import android.webkit.WebChromeClient
-import android.webkit.WebView
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
-import com.gun0912.tedpermission.PermissionListener
-import com.gun0912.tedpermission.normal.TedPermission
-import com.yjj.yjj1127.BuildConfig.VERSION_CODE
 import com.yjj.yjj1127.databinding.ActivityMainBinding
-import java.io.File
-import java.text.SimpleDateFormat
 import java.util.*
-
 
 class MainActivity : AppCompatActivity() {
 
-
-    private var photoUri: Uri? = null
     val binding : ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -48,6 +28,8 @@ class MainActivity : AppCompatActivity() {
             JavascriptBridge(),
             "Native"
         )
+
+
 
 
 
